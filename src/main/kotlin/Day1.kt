5b9e96179501203705e8fc25189882e7a2bb3569
Walop <10558717+Walop@users.noreply.github.com>
2022-12-01 17:04:33 +0200

    Day 1, part 2

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -22,5 +22,26 @@ class Day1 {
 
             return sums.max()
         }
+
+        fun process2(input: InputStream?): Int {
+            if (input == null) {
+                throw Exception("Input missing")
+            }
+
+            var i = 0
+            val sums = emptyList<Int>().toMutableList()
+            sums.add(0)
+
+            input.reader().forEachLine {
+                if (it.isEmpty()) {
+                    i++
+                    sums.add(0)
+                } else {
+                    sums[i] += it.toInt()
+                }
+            }
+
+            return sums.sortedDescending().take(3).sum()
+        }
     }
 }
--- src/test/kotlin/TestDay1.kt
+++ src/test/kotlin/TestDay1.kt
@@ -1,10 +1,18 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class TestDay1 {
-    @org.junit.jupiter.api.Test
+    @Test
     internal fun part1() {
         val input = this::class.java.classLoader.getResource("input_day1.txt")?.openStream()
         val output = Day1.process(input)
         assertEquals(72718, output)
+    }
+
+    @Test
+    internal fun part2() {
+        val input = this::class.java.classLoader.getResource("input_day1.txt")?.openStream()
+        val output = Day1.process2(input)
+        assertEquals(213089, output)
     }
 }
