33b3816a2f51b77673943093a59f68cb65f4936a
Walop <10558717+Walop@users.noreply.github.com>
2022-12-02 17:42:01 +0200

    Day 2, part 1, already losing sanity and brute forcing conditions

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,46 @@
+import java.io.InputStream
+
+class Day2 {
+    companion object {
+        private val points = mapOf("A" to 1, "X" to 1, "B" to 2, "Y" to 2, "C" to 3, "Z" to 3)
+
+        fun process(input: InputStream?): Int {
+            if (input == null) {
+                throw Exception("Input missing")
+            }
+
+            return input
+                .reader()
+                .readLines()
+                .map { it.split(" ") }
+                .map{ it.map { s -> points[s] ?: 0 }}
+                .sumOf {
+                    val win = if (it[0] == 3 && it[1] == 1) {
+                        6
+                    }
+                    else if (it[0] == 1 && it[1] == 3) {
+                        0
+                    }
+                    else if (it[0] < it[1]) {
+                        6
+                    }
+                    else if (it[0] == it[1]) {
+                        3
+                    }
+                    else {
+                        0
+                    }
+                    //println("$it, ${it[1]+win}")
+                    it[1]+win
+                }
+        }
+
+        fun process2(input: InputStream?): Int {
+            if (input == null) {
+                throw Exception("Input missing")
+            }
+            TODO("Not yet implemented")
+        }
+    }
+
+}
---
+++ src/test/kotlin/TestDay2.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TestDay2 {
+    @Test
+    internal fun part1() {
+        val input = this::class.java.classLoader.getResource("input_day2.txt")?.openStream()
+        val output = Day2.process(input)
+        assertEquals(11063, output)
+    }
+
+    @Test
+    internal fun part2() {
+        val input = this::class.java.classLoader.getResource("input_day2.txt")?.openStream()
+        val output = Day2.process2(input)
+        assertEquals(0, output)
+    }
+}
