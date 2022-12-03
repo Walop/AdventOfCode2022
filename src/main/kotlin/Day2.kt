a0aad56c3dfc873232489458ddb0ab319025d7c3
Walop <10558717+Walop@users.noreply.github.com>
2022-12-03 12:33:35 +0200

    Refactor day 2 and 3 to process input stream line by line instead of reading it all in at first

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -10,29 +10,31 @@ class Day2 {
             }
 
             return input
-                .reader()
-                .readLines()
-                .map { it.split(" ") }
-                .map{ it.map { s -> points[s] ?: 0 }}
-                .sumOf {
-                    val win = if (it[0] == 3 && it[1] == 1) {
-                        6
+                .bufferedReader()
+                .useLines { ln -> ln
+                    .map { it.split(" ") }
+                    .map{ it.map { s -> points[s] ?: 0 }}
+                    .sumOf {
+                        val win = if (it[0] == 3 && it[1] == 1) {
+                            6
+                        }
+                        else if (it[0] == 1 && it[1] == 3) {
+                            0
+                        }
+                        else if (it[0] < it[1]) {
+                            6
+                        }
+                        else if (it[0] == it[1]) {
+                            3
+                        }
+                        else {
+                            0
+                        }
+                        //println("$it, ${it[1]+win}")
+                        it[1]+win
                     }
-                    else if (it[0] == 1 && it[1] == 3) {
-                        0
-                    }
-                    else if (it[0] < it[1]) {
-                        6
-                    }
-                    else if (it[0] == it[1]) {
-                        3
-                    }
-                    else {
-                        0
-                    }
-                    //println("$it, ${it[1]+win}")
-                    it[1]+win
                 }
+
         }
 
         fun process2(input: InputStream?): Int {
@@ -41,21 +43,23 @@ class Day2 {
             }
 
             return input
-                .reader()
-                .readLines()
-                .map { it.split(" ") }
-                .map{ it.map { s -> points[s] ?: 0 }}
-                .sumOf {
-                    if (it[1] == 1) {
-                        (it[0] + 1) % 3 + 1
-                    }
-                    else if (it[1] == 2) {
-                        3 + it[0]
-                    }
-                    else {
-                        6 + (it[0] + 3) % 3 + 1
+                .bufferedReader()
+                .useLines { ln -> ln
+                    .map { it.split(" ") }
+                    .map{ it.map { s -> points[s] ?: 0 }}
+                    .sumOf {
+                        if (it[1] == 1) {
+                            (it[0] + 1) % 3 + 1
+                        }
+                        else if (it[1] == 2) {
+                            3 + it[0]
+                        }
+                        else {
+                            6 + (it[0] + 3) % 3 + 1
+                        }
                     }
                 }
+
         }
     }
 
--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -8,18 +8,20 @@ class Day3 {
             }
 
             return input
-                .reader()
-                .readLines()
-                .map {
-                    Pair(
-                        it.subSequence(0, it.length / 2).toSet(),
-                        it.subSequence(it.length / 2, it.length).toSet()
-                    )
+                .bufferedReader()
+                .useLines { ln -> ln
+                    .map {
+                        Pair(
+                            it.subSequence(0, it.length / 2).toSet(),
+                            it.subSequence(it.length / 2, it.length).toSet()
+                        )
+                    }
+                    //.map { println(it); it }
+                    .flatMap { it.first.intersect(it.second) }
+                    //.map { println(it); it }
+                    .sumOf { if (it in 'a'..'z') it.code - 96 else it.code - 38 }
                 }
-                //.map { println(it); it }
-                .flatMap { it.first.intersect(it.second) }
-                //.map { println(it); it }
-                .sumOf { if (it in 'a'..'z') it.code - 96 else it.code - 38 }
+
         }
 
         fun process2(input: InputStream?): Int {
@@ -28,11 +30,13 @@ class Day3 {
             }
 
             return input
-                .reader()
-                .readLines()
-                .chunked(3)
-                .map { it[0].toSet().intersect(it[1].toSet()).intersect(it[2].toSet()).first() }
-                .sumOf { if (it in 'a'..'z') it.code - 96 else it.code - 38 }
+                .bufferedReader()
+                .useLines { ln -> ln
+                    .chunked(3)
+                    .map { it[0].toSet().intersect(it[1].toSet()).intersect(it[2].toSet()).first() }
+                    .sumOf { if (it in 'a'..'z') it.code - 96 else it.code - 38 }
+                }
+
         }
     }
 
