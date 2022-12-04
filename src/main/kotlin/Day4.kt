87d00a60f7604fcec8aa0f23e2259073e9c9209d
Walop <10558717+Walop@users.noreply.github.com>
2022-12-04 08:29:53 +0200

    Day 4, part 1

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,26 @@
+import java.io.InputStream
+
+class Day4 {
+    companion object {
+        fun process(input: InputStream?): Int {
+            if (input == null) {
+                throw Exception("Input missing")
+            }
+
+            return input
+                .bufferedReader()
+                .useLines { ln ->
+                    ln
+                        .flatMap { it.split(',', '-') }
+                        .map { it.toInt() }
+                        .chunked(4)
+                        .count { (it[0] <= it[2] && it[1] >= it[3]) || (it[2] <= it[0] && it[3] >= it[1]) }
+                }
+        }
+
+        fun process2(input: InputStream?): Int {
+            TODO("Not yet implemented")
+        }
+    }
+
+}
---
+++ src/test/kotlin/TestDay4.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TestDay4 {
+    @Test
+    internal fun part1() {
+        val input = this::class.java.classLoader.getResource("input_day4.txt")?.openStream()
+        val output = Day4.process(input)
+        assertEquals(580, output)
+    }
+
+    @Test
+    internal fun part2() {
+        val input = this::class.java.classLoader.getResource("input_day4.txt")?.openStream()
+        val output = Day4.process2(input)
+        assertEquals(0, output)
+    }
+}
