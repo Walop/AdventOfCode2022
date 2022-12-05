c98e89af69765b0d48942ebb477d588f50a886f5
Walop <10558717+Walop@users.noreply.github.com>
2022-12-05 18:17:51 +0200

    Day 5, part 2, same crap with a temp

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -40,7 +40,47 @@ class Day5 {
         }
 
         fun process2(input: InputStream?): String {
-            TODO("Not yet implemented")
+            if (input == null) {
+                throw Exception("Input missing")
+            }
+
+            val stacks = Array(9) { Stack<Char>() }
+
+            input
+                .bufferedReader()
+                .useLines { s ->
+                    val numRegex = """\d+""".toRegex()
+                    s
+                        .onEach {
+                            if (it.firstOrNull() != 'm') it.forEachIndexed { index, c ->
+                                if (c in 'A'..'Z') stacks[(index - 1) / 4].push(
+                                    c
+                                )
+                            }
+                        }
+                        .onEach { if (it.isEmpty()) stacks.forEach { stack -> stack.reverse() } }
+                        .forEach { line ->
+                            if (line.firstOrNull() == 'm') {
+                                val nums =
+                                    numRegex.findAll(line)
+                                        .map { it.value.toInt() }.toList()
+
+                                val temp = Stack<Char>()
+
+                                repeat(nums[0]) {
+                                    temp.push(stacks[nums[1] - 1].pop())
+                                }
+
+                                //println(temp)
+
+                                repeat(nums[0]) {
+                                    stacks[nums[2] - 1].push(temp.pop())
+                                }
+                            }
+                        }
+                }
+
+            return String(stacks.mapNotNull { if (it.empty()) null else it.pop() }.toCharArray())
         }
     }
 
--- src/test/kotlin/TestDay5.kt
+++ src/test/kotlin/TestDay5.kt
@@ -13,6 +13,6 @@ class TestDay5 {
     internal fun part2() {
         val input = this::class.java.classLoader.getResource("input_day5.txt")?.openStream()
         val output = Day5.process2(input)
-        assertEquals("", output)
+        assertEquals("RBTWJWMCF", output)
     }
 }
